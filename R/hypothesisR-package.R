b8e6d6dd59c88450cb8a543ccfb70907f1f8caa6
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-07 18:08:42 -0400

    package-level code file
    I always forget this one, but it's good for creating an easy-to-find landing page in the R help window.

---
+++ R/hypothesisR-package.R
@@ -0,0 +1,8 @@
+#' hypothesisR: Wrapper for the Hypothes.is API
+#'
+#' Interact with the API for the web annotation service hypothes.is.
+#' Allows users to add, search for, and retrieve annotation data.
+#'
+#' @docType package
+#' @name hypothesisR
+NULL
