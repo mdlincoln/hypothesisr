fa4973a46890872236038aae3b43531b7d2638eb
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-08 17:26:28 -0400

    Create hs_delete

---
+++ R/hs_delete.R
@@ -0,0 +1,49 @@
+# Exported functions ----
+
+#' Delete an annotation
+#'
+#' @param token Character. Your account token, which you can generate at \url{https://hypothes.is/profile/developer}
+#' @param id Character. A hypothes.is annotation id.
+#'
+#' @return TRUE on successful deletion.
+#'
+#' @export
+hs_delete <- function(token, id) {
+  hs_delete_response <- hs_delete_handler(token, id)
+  hs_delete_results(hs_delete_response)
+}
+
+# Internal functions ----
+
+hs_delete_handler <- function(token, id) {
+
+  # Check token validity
+  is_valid_token(token)
+  is_valid_id(id)
+
+  # Format the url to post to
+  hs_base_url_list$path <- paste0("api/annotations/", id)
+  formatted_url <- httr::build_url(hs_base_url_list)
+
+  # Post the JSON data with the authorization token
+  httr::DELETE(formatted_url, httr::add_headers(Authorization = paste0("Bearer ", token)))
+}
+
+# Check the response code after deletion. If 200, return TRUE. If otherwise,
+# throw an error.
+hs_delete_results <- function(hs_delete_response) {
+  status_code <- as.character(httr::status_code(hs_delete_response))
+
+  delete_action <- function(response, code) {
+    switch (code,
+            "200" = TRUE,
+            "401" = stop("401: no API token was provided"),
+            "403" = stop("403: API token provided does not convey 'update' permissions for the annotation with the given id"),
+            "404" = stop("404: annotation with the given id was not found"),
+            stop("Hypothes.is sent an undocumented response code. Sorry!")
+    )
+  }
+
+  delete_action(hs_delete_response, status_code)
+}
+
--- tests/testthat/test_crud.R
+++ tests/testthat/test_crud.R
@@ -1,17 +1,32 @@
-context("Create hypothes.is annotations")
+context("Create, read, update, and destroy hypothes.is annotations")
 
-context("Read hypothes.is annotations")
+test_that("Annotations can be successfully created, read, updated, and destroyed.", {
+  # Because all these actions require an API token, skip on CRAN
+  skip_on_cran()
 
-test_that("Annotations can be retrieved", {
-  hs_annotation <- hs_read("Zzx_RC2cEeaSN18iqoj6Aw")
+  source("../../.Rprofile")
+  test_token <- hs_token
+  test_uri <- "https://github.com/mdlincoln/hypothesisr"
+  test_user <- "acct:mdlincoln@hypothes.is"
+  test_tags <- c("testing", "R")
+  test_text <- "Temporary annotation made for package testing."
+
+  create_id <- hs_create(token = test_token, uri = test_uri, user = test_user,
+                         tags = test_tags, text = test_text)
+
+  expect_equivalent(nchar(create_id), 22)
+  expect_error(hs_create(token = "invalid", uri = test_uri, user = test_user,
+                         tags = test_tags, text = test_text))
+
+  hs_annotation <- hs_read(create_id)
   expect_s3_class(hs_annotation, "data.frame")
   expect_equivalent(nrow(hs_annotation), 1)
+  expect_equivalent(hs_annotation$text, "Temporary annotation made for package testing.")
+
+  expect_true(hs_delete(test_token, create_id))
+  expect_error(hs_read(create_id), regexp = "404")
 })
 
 test_that("Illegal ids cause errors", {
   expect_error(hs_read("b"))
 })
-
-context("Update hypothes.is annotations")
-
-context("Delete hypothes.is annotations")
