0678dd27f2ebc344426e0d0951ef77620f912a69
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-07 23:01:39 -0400

    Test for hs_search list structure

--- tests/testthat/test_search.R
+++ tests/testthat/test_search.R
@@ -1,3 +1,6 @@
+library(httr)
+library(jsonlite)
+
 context("Search hypothes.is")
 
 test_that("Unparseable inputs cause errors.", {
@@ -7,6 +10,16 @@ test_that("Unparseable inputs cause errors.", {
   expect_error(hs_search(order = 1))
   expect_error(hs_search(limit = "a"))
   expect_error(hs_search(offset = "a"))
+})
+
+test_that("hs_search returns a list of the expected format.", {
+
+  hs_ulysses <- hs_search(text = "ulysses", limit = 5)
+
+  expect_type(hs_ulysses$total, "integer")
+  expect_type(hs_ulysses$rows, "list")
+  expect_equivalent(length(hs_ulysses$rows), 5)
+  expect_equivalent(names(hs_ulysses$rows[[1]]), c("updated", "group", "target", "links", "tags", "text", "created", "uri", "user", "document", "id", "permissions"))
 })
 
 test_that("custom fields in hs_search are correctly added to URL.", {
