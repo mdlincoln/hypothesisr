a7f30150d7083ca2f5dfa783f20a098f730a4278
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-08 17:09:07 -0400

    Basically-functioning hs_create command
    Cannot yet do anything fancy with document/target options. This just adds text annotations at the page level, unanchored to any particular text section.

---
+++ R/hs_create.R
@@ -0,0 +1,80 @@
+# Exported functions ----
+
+#' Create annotations
+#'
+#' @param token Character. Your account token, which you can generate at \url{https://hypothes.is/profile/developer}
+#' @param uri Character. The URI to be annotated.
+#' @param user Character. Your user account, normally in the format \code{acct:username@hypothes.is}
+#' @param permissions A named list with read, update, delete, and admin permissions. Defaults to setting global read permissions (\code{group:__world__}) and setting the \code{user} string for update, delete, and admin permissions.
+#' @param document A list describing the document. CURRENTLY IGNORED.
+#' @param target A list describing the highlight position of the annotation. CURRENTLY IGNORED
+#' @param tags Character. (optional) Tags to apply to the annotation.
+#' @param text Character. Text to put in the body of teh annotation. This will be coerced into a character vector of length 1 using \link{paste}.
+#'
+#' @return Upon successful creation, returns a 22-character annotation ID. This ID may be retrieved using \link{hs_read}.
+#'
+#' @export
+hs_create <- function(token, uri, user,
+                      permissions = list(read = "group:__world__",
+                                         update = user, delete = user,
+                                         admin = user),
+                      document = NULL, target = NULL, tags = NULL, text) {
+
+  hs_create_response <- hs_create_handler(token, uri, user, permissions,
+                                          document, target, tags, text)
+
+  hs_create_results(hs_create_response)
+}
+
+# Internal functions ----
+
+hs_create_handler <- function(token, uri, user, permissions, document, target, tags, text) {
+
+  # Check token validity
+  is_valid_token(token)
+
+  # Construct a list holding the annotation data, and parse into JSON
+  create_list <- list(uri = jsonlite::unbox(uri))
+  create_list$user <- jsonlite::unbox(user)
+  create_list$permissions <- permissions
+  create_list$tags <- tags
+  create_list$text <- jsonlite::unbox(text)
+  create_json <- jsonlite::toJSON(create_list)
+
+  # Format the url to post to
+  hs_base_url_list$path <- paste0("api/annotations")
+  formatted_url <- httr::build_url(hs_base_url_list)
+
+  # Post the JSON data with the authorization token
+  httr::POST(formatted_url, body = create_json, httr::accept_json(),
+    httr::content_type_json(),
+    httr::add_headers(Authorization = paste0("Bearer ", token)))
+}
+
+# Check the response code after creation. If 200, return the new annotation ID.
+# If otherwise, throw an error.
+hs_create_results <- function(hs_create_response) {
+
+  status_code <- as.character(httr::status_code(hs_create_response))
+
+  create_action <- function(response, code) {
+    switch (code,
+            "200" = list_results(response)$id,
+            "400" = stop("400: could not create annotation from your request (bad payload)"),
+            "401" = stop("401: no API token was provided"),
+            "403" = stop("403: API token provided does not convey “create” permissions"),
+            "404" = stop("404: API token is unrecognized"),
+            stop("Hypothes.is sent an undocumented response code. Sorry!")
+    )
+  }
+
+  create_action(hs_create_response, status_code)
+}
+
+# Input validation ----
+
+# Check locally if the token is a 22-character string
+is_valid_token <- function(token) {
+  if(nchar(token) != 22)
+    stop(token, " should be a 22-character string. See https://hypothes.is/profile/developer to generate your own token.")
+}
