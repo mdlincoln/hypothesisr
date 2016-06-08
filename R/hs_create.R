a25bcacdb0a9ae436a323535c37a13c743413832
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-08 17:25:56 -0400

    properly check token validity
    It should be 37 characters

--- R/hs_create.R
+++ R/hs_create.R
@@ -73,8 +73,8 @@ hs_create_results <- function(hs_create_response) {
 
 # Input validation ----
 
-# Check locally if the token is a 22-character string
+# Check locally if the token is a 37-character string
 is_valid_token <- function(token) {
-  if(nchar(token) != 22)
-    stop(token, " should be a 22-character string. See https://hypothes.is/profile/developer to generate your own token.")
+  if(nchar(token) != 37)
+    stop(token, " should be a 37-character string. See https://hypothes.is/profile/developer to generate your own token.")
 }
