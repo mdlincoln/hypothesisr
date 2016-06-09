916d7e0b7524d1fe5d6543828abd4ea1961849f7
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-08 22:11:15 -0400

    correctly switch to annotation html if incontext isn't available

--- R/hs_utils.R
+++ R/hs_utils.R
@@ -9,7 +9,13 @@ NULL
 open_context <- function(id) {
   is_valid_id(id)
   a <- hs_read(id)
-  utils::browseURL(a$links$incontext)
+  if(is.null(a$links$incontext)) {
+    warning("Annotation ", id, " has no context link. Defaulting to the annotation link.")
+    link <- a$links$html
+  } else {
+    link <- a$links$incontext
+  }
+  utils::browseURL(link)
 }
 
 #' @describeIn open_ Open an annotation on the hypothes.is webpage
